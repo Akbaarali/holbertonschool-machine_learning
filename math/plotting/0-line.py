8bca2e95308aec5f8d89b82fa19deb0dea462841
Akbaarali <12716@holbertonstudents.com>
2026-01-18 21:16:39 +0300

    Update 0-line.py

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
+"""
+0-line.py
+
+Plots y as a red line graph where y = x^3 for x in [0, 10].
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
+    """
+    Plot y = x^3 as a solid red line with x-axis ranging from 0 to 10.
+    """
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
