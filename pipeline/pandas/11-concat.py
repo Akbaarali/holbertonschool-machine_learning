6b2a8e34caff10fa48edd8f28899303025daa740
Akbaarali <12716@holbertonstudents.com>
2026-01-18 21:01:02 +0300

    Create 11-concat.py

---
+++ pipeline/pandas/11-concat.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Task 11: Concat
+Concatenate bitstamp and coinbase dataframes.
+"""
+
+import pandas as pd
+
+
+def concat(df1, df2):
+    """Concatenate df2 on top of df1 using Timestamp as index."""
+    index = __import__("10-index").index
+
+    df1 = index(df1)
+    df2 = index(df2)
+
+    df2 = df2.loc[df2.index <= 1417411920]
+
+    return pd.concat([df2, df1], keys=["bitstamp", "coinbase"])
