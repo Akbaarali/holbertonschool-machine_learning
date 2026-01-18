0c3019833bf01bbdca219be662267ed2f1e6c4a8
Akbaarali <12716@holbertonstudents.com>
2026-01-18 21:35:07 +0300

    Update 4-frequency.py

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,13 +1,27 @@
-    # bins every 10 units (0-100)
+#!/usr/bin/env python3
+"""
+4-frequency.py
+Plots a histogram of student grades.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """
+    Plots a histogram showing the frequency of student grades.
+    """
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+
+    plt.figure(figsize=(6.4, 4.8))
+
+    # Bins every 10 units from 0 to 100
     bins = np.arange(0, 101, 10)
 
     plt.hist(student_grades, bins=bins, edgecolor='black')
 
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
-    plt.title('Project A')
-
-    plt.xticks(bins)
-    plt.xlim(0, 100)
-
-    plt.show()
+    plt.title('Pr
