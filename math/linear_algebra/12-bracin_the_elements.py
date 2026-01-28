698b43769c5bc3d6e1f8fac9cc64dc19d3d43dc8
Akbaarali <12716@holbertonstudents.com>
2026-01-28 13:35:24 +0000

    Task 12: bracing the elements

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+"""Module that defines element-wise operations with NumPy arrays."""
 
 
 def np_elementwise(mat1, mat2):
+    """Return element-wise add, sub, mul, and div of mat1 and mat2."""
     add = mat1 + mat2
     sub = mat1 - mat2
     mul = mat1 * mat2
     div = mat1 / mat2
     return add, sub, mul, div
+
