eca8a9c6884e83e61aa372ad10229e02245a1e1c
Akbaarali <12716@holbertonstudents.com>
2026-01-28 17:03:17 +0400

    Update 5-across_the_planes.py

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -6,6 +6,10 @@ def add_matrices2D(mat1, mat2):
     """Add two 2D matrices element-wise and return a new matrix."""
     if len(mat1) != len(mat2):
         return None
-    if any(len(r1) != len(r2) for r1, r2 in zip(mat1, mat2)):
-        return None
-    return [[r1[i] + r2[i] for i in range(len(r1))] for r1, r2 in zip(mat1, mat2)]
+    for r1, r2 in zip(mat1, mat2):
+        if len(r1) != len(r2):
+            return None
+    new = []
+    for r1, r2 in zip(mat1, mat2):
+        new.append([a + b for a, b in zip(r1, r2)])
+    return new
