07a66ddb750820d1f48075b79bb72f96b255ec3a
Akbaarali <12716@holbertonstudents.com>
2026-01-28 18:37:49 +0400

    Update 4-frequency.py

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -13,13 +13,14 @@ def frequency():
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
 
-    bins = np.arange(0, 101, 10)
-    plt.hist(student_grades, bins=bins, edgecolor='black')
+    plt.hist(
+        student_grades,
+        bins=range(0, 101, 10),
+        edgecolor='black'
+    )
 
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
-    plt.xticks(bins)
-    plt.xlim(0, 100)
 
     plt.show()
