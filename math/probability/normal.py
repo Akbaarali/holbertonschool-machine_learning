2b833cda33ebe155cff1d987a650aaaab1309892
Akbaarali <12716@holbertonstudents.com>
2026-02-02 17:25:21 +0400

    Update normal.py

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python3
 """
-This module defines the Exponential distribution class.
+This module defines the normal distribution class.
 """
 
 class Normal:
-    """Represents an exponential distribution."""
+    """Represents an normal distribution."""
 
     def __init__(self, data=None, mean=0., stddev=1.):
+        """
+        Initializes a normal distribution.
+        """
         if data == None:
             if stddev <= 0:
                 raise ValueError("stddev must be a positive value")
             self.mean = float(mean)
             self.stddev = float(stddev)
         else:
-            if isinstance(data, list):
+            if not isinstance(data, list):
                 raise TypeError("data must be a list")
             if len(data) <= 2:
                 raise ValueError("data must contain multiple values")
