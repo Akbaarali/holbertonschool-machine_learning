c3ef3f657f83217a7885b2fe17daa5159e9abc28
Akbaarali <12716@holbertonstudents.com>
2026-02-02 12:19:58 +0400

    Update poisson.py
    task 2

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -40,13 +40,13 @@ class Poisson:
         return (e_term * (self.lambtha ** k)) / factorial
 
 
-def cdf(self,k):
-    if not isinstance(k,int):
-        k=int(k)
-    if k<0:
-        return 0
-    cem=0
-    for i in range(0,k+1):
-        cem+=self.pmf(i)
-        
-    return cem
+    def cdf(self,k):
+        if not isinstance(k,int):
+            k=int(k)
+        if k<0:
+            return 0
+        cem=0
+        for i in range(0,k+1):
+            cem+=self.pmf(i)
+            
+        return cem
