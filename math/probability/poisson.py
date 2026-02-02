fe36e203fb556222ce5548283d0e6cc32efb0043
Akbaarali <12716@holbertonstudents.com>
2026-02-02 12:17:51 +0400

    Update poisson.py
    task 2

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,4 +1,45 @@
 #!/usr/bin/env python3
+"""
+Poisson distribution
+"""
+
+
+class Poisson:
+    """Represents a Poisson distribution"""
+
+    def __init__(self, data=None, lambtha=1.):
+        """
+        Initializes a Poisson distribution
+        """
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = float(sum(data) / len(data))
+
+    def pmf(self, k):
+        """
+        Calculates the value of the PMF for a given number of successes
+        """
+        if not isinstance(k, int):
+            k = int(k)
+
+        if k < 0:
+            return 0
+
+        factorial = 1
+        for i in range(1, k + 1):
+            factorial *= i
+
+        e_term = 2.7182818285 ** (-self.lambtha)
+        return (e_term * (self.lambtha ** k)) / factorial
+
+
 def cdf(self,k):
     if not isinstance(k,int):
         k=int(k)
