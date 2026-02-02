0e87e96f11bccd8fbdd9cdb4c47aaf830e462a8a
Akbaarali <12716@holbertonstudents.com>
2026-02-02 17:52:46 +0400

    Update normal.py

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -40,3 +40,8 @@ class Normal:
         :param z: Description
         """
         return z * self.stddev + self.mean
+    
+    def pdf(self, x):
+        pi = 3.1415926536
+        exponent = -((x - self.mean) / (2 * self.stddev)) ** 2
+        return (1 / (self.stddev * (2 * pi) ** 0.5)) * (2.7182818285 ** exponent)
