2cb980c99ef6915765ca575484a5fcf5bd55d8e8
Akbaarali <12716@holbertonstudents.com>
2026-02-06 17:43:45 +0400

    Update normal.py

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -54,27 +54,15 @@ class Normal:
         """
         Calculates cumilative distribution
         """
-        if x is None:
-            return None
-
-        if not isinstance (x, (int, float)):
-            return None
-
         z = (x - self.mean) / (self.stddev * (2 ** 0.5))
-        sign  = 1
-        if z < 0:
-            sign = -1
-            z = -z
-        a1 = 0.254829592
-        a2 = -0.284496736
-        a3 = 1.421413741
-        a4 = -1.453152027
-        a5 = 1.061405429
-        p = 0.3275911
-        t = 1.0 / (1.0 + p * z)
-        e_const = 2.718281828459045
-        exp_term = e_const ** (-(z * z))
+        pi = 3.1415926536
+        two_over_sqrt_pi = 2 / (pi ** 0.5)
+        term = z
+        series_sum = term
 
-        erf_approx = 1.0 - (((((a5 * t + a4) * t + a3) * t + a2) * t + a1) * t) * exp_term
-        erf_approx *= sign
-        return 0.5 * (1.0 + erf_approx)
+        for n in range (0, 49):
+            term *= (-1) * (z * z) * (2 * n + 1) / ((n+1) * (2 * n + 3))
+            series_sum += term
+
+        erf = two_over_sqrt_pi * series_sum
+        return 0.5 * (1 + erf)
