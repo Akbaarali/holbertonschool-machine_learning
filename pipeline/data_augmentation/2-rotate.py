e06d90d212da99e1c082fde96543e17ff11faf71
Akbaarali <12716@holbertonstudents.com>
2026-04-12 16:22:43 +0300

    Update 2-rotate.py

--- pipeline/data_augmentation/2-rotate.py
+++ pipeline/data_augmentation/2-rotate.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""Rotate image 90 degrees counterclockwise"""
+"""Rotate image"""
 
 import tensorflow as tf
 
 
 def rotate_image(image):
-    """Rotates an image by 90 degrees counterclockwise"""
-    return tf.image.rot90(image)
+    """Rotates an image by 90 degrees counter-clockwise"""
+    return tf.image.rot90(image, k=1)
