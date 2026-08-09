192d0fc8d7044f707293648b2ec63f28da6c0bfb
Akbaarali <12716@holbertonstudents.com>
2026-08-09 13:10:35 +0300

    Update 1-wgan_clip.py

--- unsupervised_learning/gan/1-wgan_clip.py
+++ unsupervised_learning/gan/1-wgan_clip.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
-"""Wasserstein GAN with weight clipping."""
+"""Wasserstein Generative Adversarial Network with weight clipping."""
 
 import tensorflow as tf
 from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 class WGAN_clip(keras.Model):
-    """WGAN with clipped discriminator weights."""
+    """Wasserstein GAN using discriminator weight clipping."""
 
     def __init__(self, generator, discriminator, latent_generator,
                  real_examples, batch_size=200, disc_iter=2,
                  learning_rate=.005):
-        """Initialize the WGAN_clip model."""
+        """Initialize the Wasserstein GAN."""
         super().__init__()
 
         self.latent_generator = latent_generator
@@ -20,89 +22,140 @@ class WGAN_clip(keras.Model):
         self.discriminator = discriminator
         self.batch_size = batch_size
         self.disc_iter = disc_iter
-        self.learning_rate = learning_rate
 
+        self.learning_rate = learning_rate
         self.beta_1 = .5
         self.beta_2 = .9
 
+        # Generator loss and optimizer
         self.generator.loss = lambda x: -tf.math.reduce_mean(x)
+
         self.generator.optimizer = keras.optimizers.Adam(
             learning_rate=self.learning_rate,
             beta_1=self.beta_1,
             beta_2=self.beta_2
         )
+
         self.generator.compile(
             optimizer=self.generator.optimizer,
             loss=self.generator.loss
         )
 
-        self.discriminator.loss = lambda x, y: (
+        # Discriminator loss and optimizer
+        self.discriminator.loss = (
+            lambda x, y:
             tf.math.reduce_mean(x) - tf.math.reduce_mean(y)
         )
+
         self.discriminator.optimizer = keras.optimizers.Adam(
             learning_rate=self.learning_rate,
             beta_1=self.beta_1,
             beta_2=self.beta_2
         )
+
         self.discriminator.compile(
             optimizer=self.discriminator.optimizer,
             loss=self.discriminator.loss
         )
 
     def get_fake_sample(self, size=None, training=False):
-        """Generate fake samples."""
-        if size is None:
+        """Generate a sample of fake examples."""
+        if not size:
             size = self.batch_size
 
-        latent_sample = self.latent_generator(size)
-        return self.generator(latent_sample, training=training)
+        return self.generator(
+            self.latent_generator(size),
+            training=training
+        )
 
     def get_real_sample(self, size=None):
-        """Get random real samples."""
-        if size is None:
+        """Get a random sample of real examples."""
+        if not size:
             size = self.batch_size
 
-        indices = tf.range(tf.shape(self.real_examples)[0])
-        indices = tf.random.shuffle(indices)[:size]
+        sorted_indices = tf.range(
+            tf.shape(self.real_examples)[0]
+        )
 
-        return tf.gather(self.real_examples, indices)
+        random_indices = tf.random.shuffle(
+            sorted_indices
+        )[:size]
+
+        return tf.gather(
+            self.real_examples,
+            random_indices
+        )
 
     def train_step(self, useless_argument):
-        """Perform one WGAN training step."""
+        """Perform one Wasserstein GAN training step."""
+
         for _ in range(self.disc_iter):
             with tf.GradientTape() as tape:
                 real_sample = self.get_real_sample()
-                fake_sample = self.get_fake_sample(training=False)
+                fake_sample = self.get_fake_sample()
 
-                fake_output = self.discriminator(fake_sample, training=True)
-                real_output = self.discriminator(real_sample, training=True)
+                real_output = self.discriminator(
+                    real_sample,
+                    training=True
+                )
+
+                fake_output = self.discriminator(
+                    fake_sample,
+                    training=True
+                )
 
                 discr_loss = self.discriminator.loss(
-                    fake_output, real_output
+                    fake_output,
+                    real_output
                 )
 
             gradients = tape.gradient(
                 discr_loss,
                 self.discriminator.trainable_variables
             )
+
             self.discriminator.optimizer.apply_gradients(
-                zip(gradients, self.discriminator.trainable_variables)
+                zip(
+                    gradients,
+                    self.discriminator.trainable_variables
+                )
             )
 
-            for var in self.discriminator.trainable_variables:
-                var.assign(tf.clip_by_value(var, -1, 1))
+            # Clip discriminator weights to [-1, 1]
+            for variable in self.discriminator.trainable_variables:
+                variable.assign(
+                    tf.clip_by_value(
+                        variable,
+                        -1.0,
+                        1.0
+                    )
+                )
 
+        # Train generator once
         with tf.GradientTape() as tape:
-            fake_sample = self.get_fake_sample(training=True)
-            fake_output = self.discriminator(fake_sample, training=False)
-            gen_loss = self.generator.loss(fake_output)
+            fake_sample = self.get_fake_sample(
+                training=True
+            )
+
+            fake_output = self.discriminator(
+                fake_sample,
+                training=False
+            )
+
+            gen_loss = self.generator.loss(
+                fake_output
+            )
 
         gradients = tape.gradient(
             gen_loss,
             self.generator.trainable_variables
         )
+
         self.generator.optimizer.apply_gradients(
-            zip(gradients, self.generator.trainable_variables)
+            zip(
+                gradients,
+                self.generator.trainable_variables
+            )
         )
 
         return {
