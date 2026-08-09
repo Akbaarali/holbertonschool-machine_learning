dde21ed26a7486bfc49489eccfc218d5209c1428
Akbaarali <12716@holbertonstudents.com>
2026-08-09 13:27:51 +0300

    Update 3-generate_faces.py

--- unsupervised_learning/gan/3-generate_faces.py
+++ unsupervised_learning/gan/3-generate_faces.py
@@ -1,135 +1,163 @@
 #!/usr/bin/env python3
-"""Build convolutional generator and discriminator models."""
+"""Simple Generative Adversarial Network."""
 
+import tensorflow as tf
 from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-def convolutional_GenDiscr():
-    """Build a convolutional generator and discriminator."""
+class Simple_GAN(keras.Model):
+    """Simple Generative Adversarial Network model."""
 
-    def get_generator():
-        """Build and return the generator model."""
-        inputs = keras.Input(shape=(16,))
+    def __init__(self, generator, discriminator, latent_generator,
+                 real_examples, batch_size=200, disc_iter=2,
+                 learning_rate=.005):
+        """Initialize the Simple GAN."""
+        super().__init__()
 
-        x = keras.layers.Dense(
-            2048,
-            activation="tanh"
-        )(inputs)
+        self.latent_generator = latent_generator
+        self.real_examples = real_examples
+        self.generator = generator
+        self.discriminator = discriminator
+        self.batch_size = batch_size
+        self.disc_iter = disc_iter
 
-        x = keras.layers.Reshape(
-            (2, 2, 512)
-        )(x)
+        self.learning_rate = learning_rate
+        self.beta_1 = .5
+        self.beta_2 = .9
 
-        x = keras.layers.UpSampling2D(
-            size=(2, 2)
-        )(x)
-
-        x = keras.layers.Conv2D(
-            64,
-            kernel_size=(3, 3),
-            padding="same"
-        )(x)
-
-        x = keras.layers.BatchNormalization()(x)
-        x = keras.layers.Activation("tanh")(x)
-
-        x = keras.layers.UpSampling2D(
-            size=(2, 2)
-        )(x)
-
-        x = keras.layers.Conv2D(
-            16,
-            kernel_size=(3, 3),
-            padding="same"
-        )(x)
-
-        x = keras.layers.BatchNormalization()(x)
-        x = keras.layers.Activation("tanh")(x)
-
-        x = keras.layers.UpSampling2D(
-            size=(2, 2)
-        )(x)
-
-        x = keras.layers.Conv2D(
-            1,
-            kernel_size=(3, 3),
-            padding="same"
-        )(x)
-
-        x = keras.layers.BatchNormalization()(x)
-        outputs = keras.layers.Activation("tanh")(x)
-
-        return keras.Model(
-            inputs,
-            outputs,
-            name="generator"
+        self.generator.loss = lambda x: (
+            tf.keras.losses.MeanSquaredError()(
+                x,
+                tf.ones(x.shape)
+            )
         )
 
-    def get_discriminator():
-        """Build and return the discriminator model."""
-        inputs = keras.Input(
-            shape=(16, 16, 1)
+        self.generator.optimizer = keras.optimizers.Adam(
+            learning_rate=self.learning_rate,
+            beta_1=self.beta_1,
+            beta_2=self.beta_2
         )
 
-        x = keras.layers.Conv2D(
-            32,
-            kernel_size=(3, 3),
-            padding="same"
-        )(inputs)
-
-        x = keras.layers.MaxPooling2D(
-            pool_size=(2, 2)
-        )(x)
-
-        x = keras.layers.Activation("tanh")(x)
-
-        x = keras.layers.Conv2D(
-            64,
-            kernel_size=(3, 3),
-            padding="same"
-        )(x)
-
-        x = keras.layers.MaxPooling2D(
-            pool_size=(2, 2)
-        )(x)
-
-        x = keras.layers.Activation("tanh")(x)
-
-        x = keras.layers.Conv2D(
-            128,
-            kernel_size=(3, 3),
-            padding="same"
-        )(x)
-
-        x = keras.layers.MaxPooling2D(
-            pool_size=(2, 2)
-        )(x)
-
-        x = keras.layers.Activation("tanh")(x)
-
-        x = keras.layers.Conv2D(
-            256,
-            kernel_size=(3, 3),
-            padding="same"
-        )(x)
-
-        x = keras.layers.MaxPooling2D(
-            pool_size=(2, 2)
-        )(x)
-
-        x = keras.layers.Activation("tanh")(x)
-
-        x = keras.layers.Flatten()(x)
-
-        outputs = keras.layers.Dense(
-            1,
-            activation="tanh"
-        )(x)
-
-        return keras.Model(
-            inputs,
-            outputs,
-            name="discriminator"
+        self.generator.compile(
+            optimizer=self.generator.optimizer,
+            loss=self.generator.loss
         )
 
-    return get_generator(), get_discriminator()
+        self.discriminator.loss = lambda x, y: (
+            tf.keras.losses.MeanSquaredError()(
+                x,
+                tf.ones(x.shape)
+            )
+            +
+            tf.keras.losses.MeanSquaredError()(
+                y,
+                -1 * tf.ones(y.shape)
+            )
+        )
+
+        self.discriminator.optimizer = keras.optimizers.Adam(
+            learning_rate=self.learning_rate,
+            beta_1=self.beta_1,
+            beta_2=self.beta_2
+        )
+
+        self.discriminator.compile(
+            optimizer=self.discriminator.optimizer,
+            loss=self.discriminator.loss
+        )
+
+    def get_real_sample(self, size=None):
+        """Return a random sample of real examples."""
+        if size is None:
+            size = self.batch_size
+
+        sorted_indices = tf.range(
+            tf.shape(self.real_examples)[0]
+        )
+
+        random_indices = tf.random.shuffle(
+            sorted_indices
+        )[:size]
+
+        return tf.gather(
+            self.real_examples,
+            random_indices
+        )
+
+    def get_fake_sample(self, size=None, training=False):
+        """Return a generated sample of fake examples."""
+        if size is None:
+            size = self.batch_size
+
+        return self.generator(
+            self.latent_generator(size),
+            training=training
+        )
+
+    def train_step(self, useless_argument):
+        """Perform one training step for the GAN."""
+        for _ in range(self.disc_iter):
+
+            with tf.GradientTape() as tape:
+                real_sample = self.get_real_sample()
+                fake_sample = self.get_fake_sample()
+
+                real_output = self.discriminator(
+                    real_sample,
+                    training=True
+                )
+
+                fake_output = self.discriminator(
+                    fake_sample,
+                    training=True
+                )
+
+                discr_loss = self.discriminator.loss(
+                    real_output,
+                    fake_output
+                )
+
+            gradients = tape.gradient(
+                discr_loss,
+                self.discriminator.trainable_variables
+            )
+
+            self.discriminator.optimizer.apply_gradients(
+                zip(
+                    gradients,
+                    self.discriminator.trainable_variables
+                )
+            )
+
+        with tf.GradientTape() as tape:
+            fake_sample = self.get_fake_sample(
+                training=True
+            )
+
+            fake_output = self.discriminator(
+                fake_sample,
+                training=False
+            )
+
+            gen_loss = self.generator.loss(
+                fake_output
+            )
+
+        gradients = tape.gradient(
+            gen_loss,
+            self.generator.trainable_variables
+        )
+
+        self.generator.optimizer.apply_gradients(
+            zip(
+                gradients,
+                self.generator.trainable_variables
+            )
+        )
+
+        return {
+            "discr_loss": discr_loss,
+            "gen_loss": gen_loss
+        }
